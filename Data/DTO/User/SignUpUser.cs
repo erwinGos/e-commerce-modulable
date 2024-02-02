39857e7b69a6ac8710e95a5df9a382942ec33289
Erwin <157510632+erwinGos@users.noreply.github.com>
2024-02-02 09:49:04 +0100

    Ajout de message error dans les DTO

--- Data/DTO/User/SignUpUser.cs
+++ Data/DTO/User/SignUpUser.cs
@@ -5,12 +5,16 @@ namespace Data.DTO.User
     public class SignUpUser
     {
         [EmailAddress]
+        [Required(ErrorMessage = "Le champs email est requis.")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Le champs mot de passe est requis.")]
         public string Password { get; set; }
 
+        [Required(ErrorMessage = "Le champs Prénom est requis.")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Le champs Nom est requis.")]
         public string LastName { get; set; }
 
         public DateTime RegistrationDate { get; set; } = DateTime.Now;
