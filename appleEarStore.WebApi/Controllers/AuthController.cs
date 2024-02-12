7b0995d95c101d5f33420f7b617970e6f21c5794
Desc <157510632+erwinGos@users.noreply.github.com>
2024-02-13 00:21:55 +0100

    ajout de la methode checkAuth
    methode retournant status 200 si le token est valide

--- appleEarStore.WebApi/Controllers/AuthController.cs
+++ appleEarStore.WebApi/Controllers/AuthController.cs
@@ -2,6 +2,12 @@
 using Microsoft.AspNetCore.Mvc;
 using Data.Services.Contract;
 using Data.DTO.User;
+<<<<<<< Updated upstream
+=======
+using System.Globalization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Authorization;
+>>>>>>> Stashed changes
 
 
 namespace appleEarStore.WebApi.Controllers
@@ -17,8 +23,20 @@ namespace appleEarStore.WebApi.Controllers
             _AuthenticationService = authService;
         }
 
+<<<<<<< Updated upstream
         [HttpPost()]
         public async Task<User> SignUp(SignUpUser signUpUser)
+=======
+        [HttpPost("checkauth")]
+        [Authorize]
+        public async Task<IActionResult> CheckAuth()
+        {
+            return Ok();
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> SignUp(SignUpUser signUpUser)
+>>>>>>> Stashed changes
         {
             User user = await _AuthenticationService.SignUp(signUpUser);
             string BearerToken = _AuthenticationService.GenerateToken(user);
